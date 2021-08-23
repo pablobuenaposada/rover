a7d44858ad58de597bdbab6d20997170f5383a44
Pablo <pablo.buenaposada@gmail.com>
2021-08-23 12:15:01 +0200

    docs

--- src/models/sitter.py
+++ src/models/sitter.py
@@ -11,10 +11,15 @@ NUM_DECIMALS = 2
 
 
 def _count_distinct_letter_occurrences(string):
+    """Count how many different letters there are in a string"""
     return len(set(re.sub("[^a-zA-Z]+", "", string).lower()))
 
 
 def _calculate_profile_score(name):
+    """
+    The profile score is 5 times the fraction of the English alphabet
+    comprised by the distinct letters.
+    """
     return round(
         _count_distinct_letter_occurrences(name) * MAX_SCORE / ALPHABET_SIZE,
         NUM_DECIMALS,
@@ -22,10 +27,19 @@ def _calculate_profile_score(name):
 
 
 def _calculate_ratings_score(ratings):
+    """
+    The ratings score is the average of their stay ratings.
+    """
     return round(mean(ratings), NUM_DECIMALS)
 
 
 def _calculate_search_score(profile_score, ratings):
+    """
+    When a sitter has no stays, their search score is equal to the profile score.
+    When a sitter has 10 or more stays, their search score is equal to the ratings score.
+    When a sitter has between 1 and 9 stays is a weighted average of
+    the profile score and ratings.
+    """
     if len(ratings) >= 10:
         # more than 9 ratings then this score is the same as rating score
         return _calculate_ratings_score(ratings)
